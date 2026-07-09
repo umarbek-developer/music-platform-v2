2300f1b7a9abec692257c92b8dfadafd149e003f
Umarsobirov <umarbeksobirov0402@mail.ru>
2026-07-10 01:45:56 +0500

    codes

--- src/api/user/views/swagger.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Pastebin API')
-
-urlpatterns = [
-    re_path(r'^$', schema_view),
-]
